1a3a493da7ba2605aaf8790d0cdf8d70096b647c
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-08-24 15:08:17 +1200

    fetch data from api

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+
+type ActivityType = {
+  id: string;
+  title: string;
+  date: string;
+  description: string;
+  category: string;
+  city: string;
+  venue: string;
+};
+
+type ActivitiesType = ActivityType[];
 
 function App() {
+  const [activities, setActivities] = useState<ActivitiesType>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
+      console.log(response);
+      setActivities(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {activities.map((activity) => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
