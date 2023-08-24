99fa93a0a3c3cbdf35656e59a28b70a18b693166
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-08-25 11:03:33 +1200

    moved the mediator inside the BaseApiController

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,27 +1,17 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-
-        // make use of dependency injection in order to inject the mediator inside the API Controller class
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         //endpoint
         [HttpGet] // api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
             // sends the List Query to the mediator.Handler()
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] // api/activities/{id}
