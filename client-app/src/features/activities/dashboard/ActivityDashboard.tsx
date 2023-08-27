28ee22e95874bc4e3e32aa9ece733923beb22405
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-08-28 11:32:39 +1200

    add activity form component layout

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { Grid } from "semantic-ui-react";
 import { Activities } from "../../../app/models/activity";
 import { ActivityDetails } from "../details/ActivityDetails";
+import { ActivityForm } from "../form/ActivityForm";
 import { ActivityList } from "./ActivityList";
 
 export const ActivityDashboard: FC<{ activities: Activities }> = ({
@@ -14,6 +15,7 @@ export const ActivityDashboard: FC<{ activities: Activities }> = ({
       </Grid.Column>
       <Grid.Column width="6">
         {activities[0] && <ActivityDetails activity={activities[0]} />}
+        <ActivityForm activity={activities[0]} />
       </Grid.Column>
     </Grid>
   );
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import { Button, Form, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+export const ActivityForm: FC<{ activity: Activity }> = ({ activity }) => {
+  return (
+    <Segment clearing>
+      <Form>
+        <Form.Input placeholder="Title" />
+        <Form.TextArea placeholder="Description" />
+        <Form.Input placeholder="Category" />
+        <Form.Input placeholder="Date" />
+        <Form.Input placeholder="City" />
+        <Form.Input placeholder="Venue" />
+        <Button floated="right" positive type="submit" content="Submit" />
+        <Button floated="right" type="button" content="Cancel" />
+      </Form>
+    </Segment>
+  );
+};
