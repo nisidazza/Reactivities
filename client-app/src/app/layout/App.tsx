37d313c3f2241bd28f9cb21a71bb3a701dc1b6ab
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-08-28 10:29:40 +1200

    folders restructure

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Header, List, ListItem } from "semantic-ui-react";
-import "./App.css";
 
 type ActivityType = {
   id: string;
