ab96eadccffa30dcc7195c1d8ab4b08ef8ad0941
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-08-28 16:02:54 +1200

    fix update request

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -34,7 +34,7 @@ const ActivitiesRequests = {
   details: (id: string) => requests.get<Activity>(`/activities/${id}`),
   create: (activity: Activity) => axios.post<void>("activities/", activity),
   update: (activity: Activity) =>
-    axios.put<void>(`/activities/${activity}`, activity),
+    axios.put<void>(`/activities/${activity.id}`, activity),
   delete: (id: string) => axios.delete<void>(`/activities/${id}`),
 };
 
