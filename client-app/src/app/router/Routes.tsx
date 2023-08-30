d3ed8f1b264794e1b32d92dd1598932000676dcd
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-08-30 15:39:12 +1200

    add children to main route

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,12 +1,28 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
+import { ActivityForm } from "../../features/activities/form/ActivityForm";
+import { HomePage } from "../../features/home/HomePage";
 import App from "../layout/App";
 
-export const routes:RouteObject[] = [
-    {
-        path:'/',
-        element: <App />,
-
-    }
-]
+export const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "activities",
+        element: <ActivityDashboard />,
+      },
+      {
+        path: "createActivity",
+        element: <ActivityForm />,
+      },
+    ],
+  },
+];
 
 export const router = createBrowserRouter(routes);
