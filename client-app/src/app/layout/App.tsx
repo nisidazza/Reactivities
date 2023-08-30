10c10298c4a93462b3e3f33c15dfc3d016334922
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-08-30 15:39:59 +1200

    refactoring to load activities in the actvities route page

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,26 +1,14 @@
 import { observer } from "mobx-react-lite";
-import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import { Container } from "semantic-ui-react";
-import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
-import { useStore } from "../stores/store";
-import { LoadingComponent } from "./LoadingComponent";
 import { NavBar } from "./NavBar";
 
 const App = () => {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial)
-    return <LoadingComponent content="Loading app" />;
-
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard />
+        <Outlet />
       </Container>
     </>
   );
