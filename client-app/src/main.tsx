5e9f9fba3a2ce2071e9e4661dd006841d5b49ea9
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-08-30 15:31:51 +1200

    set up path route for App and add RouterProvider/>

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
-import App from "./app/layout/App";
 import "./app/layout/styles.css";
+import { router } from "./app/router/Routes";
 import { StoreContext, store } from "./app/stores/store";
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   // <React.StrictMode>
   <StoreContext.Provider value={store}>
-    <App />
+    <RouterProvider router={router} />
   </StoreContext.Provider>
   // </React.StrictMode>
 );
