cff6bc022129efd7169258a8cd7bfcc25ff11b13
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-01 13:52:22 +1200

    refactoring current form with Formik

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,8 +1,8 @@
-import { Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import { observer } from "mobx-react-lite";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Button, Form, Segment } from "semantic-ui-react";
+import { Button, Segment } from "semantic-ui-react";
 import { LoadingComponent } from "../../../app/layout/LoadingComponent";
 import { Activity } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
@@ -62,44 +62,13 @@ export const ActivityForm = observer(() => {
         onSubmit={(values) => console.log(values)}
       >
         {({ values: activity, handleChange, handleSubmit }) => (
-          <Form onSubmit={handleSubmit} autoComplete="off">
-            <Form.Input
-              placeholder="Title"
-              value={activity.title}
-              name="title"
-              onChange={handleChange}
-            />
-            <Form.TextArea
-              placeholder="Description"
-              value={activity.description}
-              name="description"
-              onChange={handleChange}
-            />
-            <Form.Input
-              placeholder="Category"
-              value={activity.category}
-              name="category"
-              onChange={handleChange}
-            />
-            <Form.Input
-              placeholder="Date"
-              value={activity.date}
-              name="date"
-              onChange={handleChange}
-              type="date"
-            />
-            <Form.Input
-              placeholder="City"
-              value={activity.city}
-              name="city"
-              onChange={handleChange}
-            />
-            <Form.Input
-              placeholder="Venue"
-              value={activity.venue}
-              name="venue"
-              onChange={handleChange}
-            />
+          <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+            <Field placeholder="Title" name="title" />
+            <Field placeholder="Description" name="description" />
+            <Field placeholder="Category" name="category" />
+            <Field placeholder="Date" name="date" type="date" />
+            <Field placeholder="City" name="city" />
+            <Field placeholder="Venue" name="venue" />
             <Button
               loading={loading}
               floated="right"
