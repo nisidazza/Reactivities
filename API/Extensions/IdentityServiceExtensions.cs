21f0dfe899d052a2819b2cf907d883c95f124fa5
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-04 16:55:09 +1200

    configure Identity in the startup class

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,23 @@
+
+
+using Domain;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false
+            })
+            .AddEntityFrameworkStores > DataContext > (); // allows us to query the users in the Entity Framework Store or database
+
+            services.AddAuthentication();
+
+            return services;
+        }
+    }
+}
