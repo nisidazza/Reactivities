aa09877f9313237f5dd69e8b549039e7f3ffa4df
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-04 17:28:25 +1200

    add account controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+using API.DTOs;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+            // the user logs in with username and password 
+            // a token is returned
+
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized();
+
+            var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (result)
+            {
+                return new UserDto
+                {
+                    DisplayName = user.DisplayName,
+                    Image = null,
+                    Token = "this will be a toke",
+                    Username = user.UserName
+,
+                };
+            }
+            return Unauthorized();
+        }
+    }
+}
