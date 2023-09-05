79ecedb43abb1e224c4e35b7a1b7d7a9ee343c96
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-05 14:18:39 +1200

    implement logic to register new user checking if username and email already exist or not

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -44,6 +45,45 @@ namespace API.Controllers
                 };
             }
             return Unauthorized();
+        }
+
+        [HttpPost("register")]
+
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            //check if the Db has duplicate username
+            if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
+            {
+                return BadRequest("Username is already taken");
+            }
+
+            if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+            {
+                return BadRequest("Email is already taken");
+            }
+
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.Username,
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (result.Succeeded)
+            {
+                return new UserDto
+                {
+                    DisplayName = user.DisplayName,
+                    Image = null,
+                    Token = _tokenService.CreateToken(user),
+                    Username = user.UserName,
+
+                };
+            }
+
+            return BadRequest(result.Errors);
         }
     }
 }
