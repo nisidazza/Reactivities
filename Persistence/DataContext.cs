72910f3506cc245aebafc822c49815c3fce1b202
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-06 14:56:39 +1200

    configure many to many relationship

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,5 +12,26 @@ namespace Persistence
         }
 
         public DbSet<Activity> Activities { get; set; } // it represents the table name once the DB is created
+
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; } // useful if we want to get an attendee but not an activity
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // make up a priamry key that it's a combination of user Id and activity Id
+            builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
+
+            //configure the entity itself
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.AppUser)
+                .WithMany(a => a.Activities)
+                .HasForeignKey(aa => aa.AppUserId);
+
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.Activity)
+                .WithMany(a => a.Attendees)
+                .HasForeignKey(aa => aa.ActivityId);
+        }
     }
 }
