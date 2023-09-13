6bef933833d3edb1cf2d1af7f4699e4b9fb701df
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-13 15:15:57 +1200

    update mapping profile to include mapping from user obj to profile

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -15,6 +15,9 @@ namespace Application.Core
             CreateMap<ActivityAttendee, Profiles.Profile>().ForMember(d => d.DisplayName, opt => opt.MapFrom(s => s.AppUser.DisplayName))
             .ForMember(d => d.Username, opt => opt.MapFrom(s => s.AppUser.UserName))
             .ForMember(d => d.Bio, opt => opt.MapFrom(s => s.AppUser.Bio));
+
+            CreateMap<AppUser, Profiles.Profile>()
+            .ForMember(d => d.Image, opt => opt.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
