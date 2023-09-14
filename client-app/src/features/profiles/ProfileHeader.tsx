f1440be5fad3fb70f344648e2d4aa1e7d115d136
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-15 11:08:53 +1200

    display profile info

--- client-app/src/features/profiles/ProfileHeader.tsx
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -1,3 +1,5 @@
+import { observer } from "mobx-react-lite";
+import { FC } from "react";
 import {
   Button,
   Divider,
@@ -8,42 +10,49 @@ import {
   Segment,
   Statistic,
 } from "semantic-ui-react";
+import { Profile } from "../../app/models/profile";
 
-export const ProfileHeader = () => {
-  return (
-    <Segment>
-      <Grid>
-        <Grid.Column width={12}>
-          <Item.Group>
-            <Item>
-              <Item.Image avatar size="small" src={"/assets/user.png"} />
-              <Item.Content verticalAlign="middle">
-                <Header as="h1" content="Display Name" />
-              </Item.Content>
-            </Item>
-          </Item.Group>
-        </Grid.Column>
-        <Grid.Column width={4}>
-          <Statistic.Group widths={2}>
-            <Statistic label="Followers" value="5" />
-            <Statistic label="Following" value="32" />
-          </Statistic.Group>
-          <Divider />
-          <Reveal animated="move">
-            <Reveal.Content visible style={{ width: "100%" }}>
-              <Button fluid color="teal" content="Following" />
-            </Reveal.Content>
-            <Reveal.Content hidden style={{ width: "100%" }}>
-              <Button
-                fluid
-                basic
-                color={true ? "red" : "green"}
-                content={true ? "Unfollow" : "Follow"}
-              />
-            </Reveal.Content>
-          </Reveal>
-        </Grid.Column>
-      </Grid>
-    </Segment>
-  );
-};
+export const ProfileHeader: FC<{ profile: Profile }> = observer(
+  ({ profile }) => {
+    return (
+      <Segment>
+        <Grid>
+          <Grid.Column width={12}>
+            <Item.Group>
+              <Item>
+                <Item.Image
+                  avatar
+                  size="small"
+                  src={profile.image || "/assets/user.png"}
+                />
+                <Item.Content verticalAlign="middle">
+                  <Header as="h1" content={profile.displayName} />
+                </Item.Content>
+              </Item>
+            </Item.Group>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <Statistic.Group widths={2}>
+              <Statistic label="Followers" value="5" />
+              <Statistic label="Following" value="32" />
+            </Statistic.Group>
+            <Divider />
+            <Reveal animated="move">
+              <Reveal.Content visible style={{ width: "100%" }}>
+                <Button fluid color="teal" content="Following" />
+              </Reveal.Content>
+              <Reveal.Content hidden style={{ width: "100%" }}>
+                <Button
+                  fluid
+                  basic
+                  color={true ? "red" : "green"}
+                  content={true ? "Unfollow" : "Follow"}
+                />
+              </Reveal.Content>
+            </Reveal>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    );
+  }
+);
