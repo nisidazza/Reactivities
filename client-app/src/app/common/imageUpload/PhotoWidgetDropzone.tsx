8ce15d00ec2c2efd9c74d61f337d71389a28faa6
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-15 12:01:28 +1200

    add image dropzone

---
+++ client-app/src/app/common/imageUpload/PhotoWidgetDropzone.tsx
@@ -0,0 +1,43 @@
+import { FC, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { Header, Icon } from "semantic-ui-react";
+
+export const PhotoWidgetDropzone: FC<{ setFiles: (files: any) => void }> = ({
+  setFiles,
+}) => {
+  const dzStyles = {
+    border: "dashed 3px #eee",
+    borderColor: "#eee",
+    borderRadius: "5px",
+    paddingTop: "30px",
+    textAlign: "center" as "center",
+    height: "200px",
+  };
+
+  const dzActive = {
+    borderColor: "green",
+  };
+
+  const onDrop = useCallback(
+    (acceptedFiles: any) => {
+      setFiles(
+        acceptedFiles.map((file: any) =>
+          Object.assign(file, { preview: URL.createObjectURL(file) })
+        )
+      );
+    },
+    [setFiles]
+  );
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div
+      {...getRootProps()}
+      style={isDragActive ? { ...dzStyles, ...dzActive } : dzStyles}
+    >
+      <input {...getInputProps()} />
+      <Icon name="upload" size="huge" />
+      <Header content="Drop image here" />
+    </div>
+  );
+};
