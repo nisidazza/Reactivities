b9bfc7e749f10bcc25942f83164c4ee9fec66d86
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-19 10:39:19 +1200

    add commentDto and mapping

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,4 +1,5 @@
 using Application.Activities;
+using Application.Comments;
 using AutoMapper;
 using Domain;
 
@@ -9,16 +10,23 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
-            CreateMap<Activity, ActivityDto>().ForMember(destination => destination.HostUsername,
+            CreateMap<Activity, ActivityDto>()
+            .ForMember(destination => destination.HostUsername,
                 opt => opt.MapFrom(s => s.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
 
-            CreateMap<ActivityAttendee, AttendeeDto>().ForMember(d => d.DisplayName, opt => opt.MapFrom(s => s.AppUser.DisplayName))
+            CreateMap<ActivityAttendee, AttendeeDto>()
+            .ForMember(d => d.DisplayName, opt => opt.MapFrom(s => s.AppUser.DisplayName))
             .ForMember(d => d.Username, opt => opt.MapFrom(s => s.AppUser.UserName))
             .ForMember(d => d.Bio, opt => opt.MapFrom(s => s.AppUser.Bio))
             .ForMember(d => d.Image, opt => opt.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
 
             CreateMap<AppUser, Profiles.Profile>()
             .ForMember(d => d.Image, opt => opt.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
+
+            CreateMap<Comment, CommentDto>()
+            .ForMember(d => d.DisplayName, opt => opt.MapFrom(s => s.Author.DisplayName))
+            .ForMember(d => d.Username, opt => opt.MapFrom(s => s.Author.UserName))
+            .ForMember(d => d.Image, opt => opt.MapFrom(s => s.Author.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
