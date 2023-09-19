26db241d79d6d859b2a36f44b4ed0e4373a69ab1
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-20 11:25:24 +1200

    update Profile class and profile mapping

--- Application/Profiles/Profile.cs
+++ Application/Profiles/Profile.cs
@@ -12,6 +12,12 @@ namespace Application.Profiles
 
         public string Image { get; set; }
 
+        public bool Following { get; set; } // check if current user is following a particular user
+
+        public int FollowersCount { get; set; }
+
+        public int FollowingCount { get; set; }
+
         public ICollection<Photo> Photos { get; set; }
     }
 }
