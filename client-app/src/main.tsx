d054b798cb772d7258929e6be5f5d1514c926454
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-19 15:30:35 +1200

    install semantic-ui-react@3.0.0-beta.0 to fix findDOMNode warning

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -7,14 +7,15 @@ import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
 import { router } from "./app/router/Routes";
 import { StoreContext, store } from "./app/stores/store";
+import React from "react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-  <StoreContext.Provider value={store}>
-    <RouterProvider router={router} />
-  </StoreContext.Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <StoreContext.Provider value={store}>
+      <RouterProvider router={router} />
+    </StoreContext.Provider>
+  </React.StrictMode>
 );
