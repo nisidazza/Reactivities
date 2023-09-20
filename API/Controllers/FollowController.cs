ad42f9142f2c5bcfb19ac9f473f5e38adb352141
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-20 13:52:25 +1200

    add Handler to return a List of followers and get endpoint

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -11,5 +11,13 @@ namespace API.Controllers
         {
             return HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUsername = username }));
         }
+
+        [HttpGet("{username}")]
+
+        // username is coming from the route parameter, the predicate from the query string
+        public async Task<IActionResult> GetFollowing(string username, string predicate)
+        {
+            return HandleResult(await Mediator.Send(new List.Query { Username = username, Predicate = predicate }));
+        }
     }
 }
---
+++ Application/Followers/List.cs
@@ -0,0 +1,55 @@
+using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Followers
+{
+    public class List
+    {
+        public class Query : IRequest<Result<List<Profiles.Profile>>>
+        {
+            //do we want to return a list of followers or a list of user that is following that user?
+            public string Predicate { get; set; }
+
+            public string Username { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<Profiles.Profile>>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _mapper = mapper;
+                _context = context;
+
+            }
+            public async Task<Result<List<Profiles.Profile>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var profiles = new List<Profiles.Profile>();
+
+                switch (request.Predicate)
+                {
+                    case "followers":
+                        profiles = await _context.UserFollowings.Where(x => x.Target.UserName == request.Username)
+                        .Select(u => u.Observer)
+                        .ProjectTo<Profiles.Profile>(_mapper.ConfigurationProvider)
+                        .ToListAsync();
+                        break;
+                    case "following":
+                        profiles = await _context.UserFollowings.Where(x => x.Observer.UserName == request.Username)
+                      .Select(u => u.Target)
+                      .ProjectTo<Profiles.Profile>(_mapper.ConfigurationProvider)
+                      .ToListAsync();
+                        break;
+                }
+
+                return Result<List<Profiles.Profile>>.Success(profiles);
+
+            }
+        }
+    }
+}
