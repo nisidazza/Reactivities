3d9adecb57f35aa5754ab65127ac431c2d53ef6d
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-09-22 15:07:07 +1200

    add infinite scrolling

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,29 +1,52 @@
 import { observer } from "mobx-react-lite";
-import { useEffect } from "react";
-import { Grid } from "semantic-ui-react";
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroller";
+import { Grid, Loader } from "semantic-ui-react";
 import { LoadingComponent } from "../../../app/layout/LoadingComponent";
+import { PagingParams } from "../../../app/models/pagination";
 import { useStore } from "../../../app/stores/store";
 import { ActivityFilters } from "./ActivityFilters";
 import { ActivityList } from "./ActivityList";
 
 export const ActivityDashboard = observer(() => {
   const { activityStore } = useStore();
-  const { loadActivities } = activityStore;
+  const { loadActivities, pagination, setPagingParams } = activityStore;
+  const [loadingNext, setLoadingNext] = useState(false);
+
+  const handleGetNext = () => {
+    setLoadingNext(true);
+    setPagingParams(new PagingParams(pagination!.currentPage + 1));
+    loadActivities().then(() => setLoadingNext(false));
+  };
 
   useEffect(() => {
     loadActivities();
   }, [loadActivities]);
 
-  if (activityStore.loadingInitial)
+  if (activityStore.loadingInitial && !loadingNext)
     return <LoadingComponent content="Loading app" />;
 
   return (
     <Grid>
       <Grid.Column width="10">
-        <ActivityList />
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={handleGetNext}
+          hasMore={
+            !loadingNext &&
+            !!pagination &&
+            pagination.currentPage < pagination.totalPages
+          }
+          initialLoad={false}
+        >
+          <ActivityList />
+        </InfiniteScroll>
       </Grid.Column>
       <Grid.Column width="6">
         <ActivityFilters />
+      </Grid.Column>
+      <Grid.Column width={10}>
+        <Loader active={loadingNext} />
       </Grid.Column>
     </Grid>
   );
