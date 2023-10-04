e39b35f3eefbb5bb316bc56b92dafef60e0f5fc9
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-10-04 13:37:02 +1300

    udpdate userStore with refresh token functionality

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -6,6 +6,7 @@ import { store } from "./store";
 
 export default class UserStore {
   user: User | null = null;
+  refreshTokenTimeout?: number;
 
   constructor() {
     makeAutoObservable(this);
@@ -18,6 +19,7 @@ export default class UserStore {
   login = async (creds: UserFormValues) => {
     const user = await agent.AccountRequests.login(creds);
     store.commonStore.setToken(user.token);
+    this.startRefreshTokenTimer(user);
     runInAction(() => (this.user = user));
     router.navigate("/activities");
     store.modalStore.closeModal();
@@ -32,6 +34,8 @@ export default class UserStore {
   getUser = async () => {
     try {
       const user = await agent.AccountRequests.current();
+      store.commonStore.setToken(user.token);
+      this.startRefreshTokenTimer(user);
       runInAction(() => (this.user = user));
     } catch (error) {
       console.log(error);
@@ -41,6 +45,7 @@ export default class UserStore {
   register = async (creds: UserFormValues) => {
     const user = await agent.AccountRequests.register(creds);
     store.commonStore.setToken(user.token);
+    this.startRefreshTokenTimer(user);
     runInAction(() => (this.user = user));
     router.navigate("/activities");
     store.modalStore.closeModal();
@@ -52,5 +57,31 @@ export default class UserStore {
 
   setDisplayName = (displayName: string) => {
     if (this.user) this.user.displayName = displayName;
+  };
+
+  refreshToken = async () => {
+    this.stopRefreshTokenTimer();
+    try {
+      const user = await agent.AccountRequests.refreshToken();
+      runInAction(() => (this.user = user));
+      store.commonStore.setToken(user.token);
+      this.startRefreshTokenTimer(user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // figuring out when the token expires and 30 secs before then we try to refresh the token
+  // The atob() function decodes a string of data which has been encoded using Base64 encoding
+  private startRefreshTokenTimer = (user: User) => {
+    const jwtToken = JSON.parse(atob(user.token.split(".")[1])); // extract the data out of the token we are interested in, its expiry
+    const expires = new Date(jwtToken.exp * 1000);
+    const timeout = expires.getTime() - Date.now() - 30 * 1000;
+    this.refreshTokenTimeout = setTimeout(this.refreshToken, timeout);
+    console.log({ refreshTimeout: this.refreshTokenTimeout });
+  };
+
+  private stopRefreshTokenTimer = () => {
+    clearTimeout(this.refreshTokenTimeout);
   };
 }
